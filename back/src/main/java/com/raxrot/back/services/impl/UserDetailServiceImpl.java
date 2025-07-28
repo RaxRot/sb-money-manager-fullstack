490409a4e1ef1ba1123286581d33d5dafcd64df1
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-28 19:25:10 +0100

    add UserDetailServiceImpl

---
+++ back/src/main/java/com/raxrot/back/services/impl/UserDetailServiceImpl.java
@@ -0,0 +1,29 @@
+package com.raxrot.back.services.impl;
+
+import com.raxrot.back.entities.User;
+import com.raxrot.back.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return UserDetailsImpl.builder()
+                .id(user.getId())
+                .fullName(user.getFullName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
