349f97bbed9827c62978f0d15d820b775fe9413f
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-28 22:34:37 +0100

    add Expense/Income DTOs validation

--- back/src/main/java/com/raxrot/back/dtos/IncomeRequestDTO.java
+++ back/src/main/java/com/raxrot/back/dtos/IncomeRequestDTO.java
@@ -1,5 +1,8 @@
 package com.raxrot.back.dtos;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +15,15 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Builder
 public class IncomeRequestDTO {
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
     private String icon;
+
+    @NotNull(message = "Category ID is required")
     private Long categoryId;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.01", message = "Amount must be greater than zero")
     private BigDecimal amount;
 }
