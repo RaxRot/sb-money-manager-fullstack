132a37bbaf7926976b127e06b9ae7a6a280ec0e2
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-29 15:49:17 +0100

    add AppConstants

---
+++ back/src/main/java/com/raxrot/back/configurations/AppConstants.java
@@ -0,0 +1,12 @@
+package com.raxrot.back.configurations;
+
+public final class AppConstants {
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String CATEGORY_ALREADY_EXISTS = "Category already exists";
+    public static final String CATEGORY_NOT_FOUND = "Category not found";
+    public static final String INCOME_NOT_FOUND = "Income not found";
+    public static final String EXPENSE_NOT_FOUND = "Expense not found";
+    public static final String PERMISSION_DENIED = "You do not have permission to delete this item";
+    public static final String EMAIL_IN_USE = "Email address already in use";
+    public static final String ACCOUNT_NOT_ACTIVATED = "Account not activated";
+}
