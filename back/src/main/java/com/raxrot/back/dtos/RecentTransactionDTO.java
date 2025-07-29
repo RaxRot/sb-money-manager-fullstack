a7e40d759c80098baad1acbc92a2a2e57d37273b
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-29 14:04:37 +0100

    add ResentTransactionDTO

---
+++ back/src/main/java/com/raxrot/back/dtos/RecentTransactionDTO.java
@@ -0,0 +1,26 @@
+package com.raxrot.back.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecentTransactionDTO {
+    private Long id;
+    private Long profileId;
+    private String name;
+    private String icon;
+    private BigDecimal amount;
+    private LocalDate date;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String type; // "income" или "expense"
+}
