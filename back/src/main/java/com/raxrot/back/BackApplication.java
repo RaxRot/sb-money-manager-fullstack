14b5ab2e9c273e709f2b059a7d581c2208ff3276
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-29 14:38:43 +0100

    add EnableScheduling for Notifications

--- back/src/main/java/com/raxrot/back/BackApplication.java
+++ back/src/main/java/com/raxrot/back/BackApplication.java
@@ -2,12 +2,13 @@ package com.raxrot.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
     }
-
 }
