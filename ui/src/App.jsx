d028ba05818a7a79a8fcd06564fdeb91ca844eb3
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-07-29 16:43:02 +0100

    add BrowserRouter Routes Route

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Income from "./pages/Income.jsx";
+import Expense from "./pages/Expense.jsx";
+import Category from "./pages/Category.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Filter from "./pages/Filter.jsx";
 
 function App() {
 
   return (
     <>
-      <div className="text-3xl font-bold underline">Money Manager</div>
+
+
+     <BrowserRouter>
+         <Routes>
+             <Route path="/dashboard" element={<Home/>}/>
+             <Route path="/income" element={<Income/>}/>
+             <Route path="/expense" element={<Expense/>}/>
+             <Route path="/category" element={<Category/>}/>
+             <Route path="/filter" element={<Filter/>}/>
+             <Route path="/login" element={<Login/>}/>
+             <Route path="/signup" element={<SignUp/>}/>
+         </Routes>
+     </BrowserRouter>
     </>
   )
 }
